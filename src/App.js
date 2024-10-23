207f7145f81e6a16fd8df8817ae71d485282be7a
David-Good-Enough <124247495+David-Good-Enough@users.noreply.github.com>
2024-10-23 09:16:36 +0200

    {~} app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <UserProvider>
         <Router>
             <div>
-                <Navbar />  {/* Placer le composant Navbar ici */}
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
